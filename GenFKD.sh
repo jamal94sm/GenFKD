039771ec3f14230581f764d1170efa455aa9d26f
Jamal Seyedmohammadi <44075279+jamal94sm@users.noreply.github.com>
2025-09-14 10:31:42 +0330

    Update GenFKD.sh

--- GenFKD.sh
+++ GenFKD.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 #SBATCH --account=def-arashmoh
 #SBATCH --nodes=1
+#SBATCH --gpus=a100:1  # Graham: t4 or v100 or a100 or dgx or a5000 or h100; Narval: a100, a100_4g.20g; Cedar: p100, p100l, v100l, a40
+#SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4 # 8, 16
-#SBATCH --mem=32G               # memory per node (ex: 16G) you can get more 
-#SBATCH --time=01:00:00 		   # time period you need for your code (it is 12 hours for example)
+#SBATCH --mem=40G               # memory per node (ex: 16G) you can get more 
+#SBATCH --time=10:00:00 		   # time period you need for your code (it is 12 hours for example)
 #SBATCH --mail-user=<jamal73sm@gmail.com> 	# replace with your email address to get emails to know when it is started or failed. 
 #SBATCH --mail-type=ALL
 
-#cd /home/shahab33/projects/def-arashmoh/shahab33/FeDK2P
-cd /project/def-arashmoh/shahab33/Rohollah/projects/FeDK2P/FeDK2P
+
+#cd /home/shahab33/projects/def-arashmoh/shahab33/FeD2P #Cedar
+
+#cd /project/def-arashmoh/shahab33/Rohollah/projects/FeD2P #Graham
+
+cd /project/def-arashmoh/shahab33/GenFKD #Narval 
 
 
 module purge
 module load python
 module load cuda
 
-#source ~/FeDK2P/bin/activate  	# activate your environment
-source /project/def-arashmoh/shahab33/Rohollah/projects/FeDK2P/FeDK2P/fedk2p/bin/activate 
 
-python fedk2p.py   	# this is the direction and the name of your code
+#source /home/shahab33/FeDK2P/bin/activate  	# Cedar
+
+#source /home/shahab33/fed2p/bin/activate #Graham
+
+source /home/shahab33/fed2p/bin/activate #Narval
+
+#python main1.py --local_model_name "ResNet20" --num_train_samples 10000 --alpha_dirichlet 10 --output_name "ResNet20_10K_alpha10_"  	# this is the direction and the name of your code
+python main.py
+
+#python openVocab.py
