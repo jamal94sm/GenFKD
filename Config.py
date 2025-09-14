204df75d1243896163720927f2d7807a4b52688c
Jamal Seyedmohammadi <44075279+jamal94sm@users.noreply.github.com>
2025-09-14 11:09:11 +0330

    Update Config.py

--- Config.py
+++ Config.py
@@ -20,7 +20,7 @@ def get_args():
     parser.add_argument('--num_test_samples', type=int, default=3000)
     parser.add_argument('--dataset', type=str, default="cifar10")
     parser.add_argument('--Foundation_model', type=str, default="openai/clip-vit-base-patch32")
-    parser.add_argument('--rounds', type=int, default=10)
+    parser.add_argument('--rounds', type=int, default=50)
     parser.add_argument('--local_learning_rate', type=float, default=0.001)
     parser.add_argument('--local_batch_size', type=int, default=64)
     parser.add_argument('--local_epochs', type=int, default=1)
