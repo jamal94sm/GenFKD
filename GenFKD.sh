e4b8c2253f9cbf7f3430d708d73c0079cfa0e02b
Jamal Seyedmohammadi <44075279+jamal94sm@users.noreply.github.com>
2025-09-15 20:24:30 +0330

    Update GenFKD.sh

--- GenFKD.sh
+++ GenFKD.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --account=def-arashmoh
 #SBATCH --nodes=1
-#SBATCH --gpus=h100:1  # Graham: t4 or v100 or a100 or dgx or a5000 or h100; Narval: a100, a100_4g.20g; Cedar: p100, p100l, v100l, a40
+#SBATCH --gpus-per-node=v100:1       # or a100:1, a5000:1, dgx:1, t4:1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=4 # 8, 16
-#SBATCH --mem=32G               # memory per node (ex: 16G) you can get more 
-#SBATCH --time=5:00:00 		   # time period you need for your code (it is 12 hours for example)
-#SBATCH --mail-user=<jamal73sm@gmail.com> 	# replace with your email address to get emails to know when it is started or failed. 
+#SBATCH --cpus-per-task=4            # adjust (e.g., 8 or 16 if needed)
+#SBATCH --mem=32G                    # memory per node
+#SBATCH --time=5:00:00               # job time limit (HH:MM:SS)
+#SBATCH --mail-user=jamal73sm@gmail.com
 #SBATCH --mail-type=ALL
 
 
