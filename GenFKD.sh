f5d8e23a94c3ed7cd26831cc18f7d23a26edb6bb
Jamal Seyedmohammadi <44075279+jamal94sm@users.noreply.github.com>
2025-09-16 11:15:12 +0330

    Update GenFKD.sh

--- GenFKD.sh
+++ GenFKD.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 #SBATCH --account=def-arashmoh
 #SBATCH --nodes=1
-#SBATCH --gpus-per-node=v100:1       # or a100:1, a5000:1, dgx:1, t4:1
+#SBATCH --gpus-per-node=a100:1       # Narval: a100, a100_4g.20g; Cedar: p100, p100l, v100l, a40
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4            # adjust (e.g., 8 or 16 if needed)
-#SBATCH --mem=32G                    # memory per node
-#SBATCH --time=5:00:00               # job time limit (HH:MM:SS)
+#SBATCH --mem=40G                    # memory per node
+#SBATCH --time=12:00:00               # job time limit (HH:MM:SS)
 #SBATCH --mail-user=jamal73sm@gmail.com
 #SBATCH --mail-type=ALL
 
 
 #cd /home/shahab33/projects/def-arashmoh/shahab33/GenFKD #Cedar
 
-cd /project/def-arashmoh/shahab33/Rohollah/projects/FeD2P #Graham
+#cd /project/def-arashmoh/shahab33/Rohollah/projects/FeD2P #Graham
 
-#cd /project/def-arashmoh/shahab33/GenFKD #Narval 
+cd /project/def-arashmoh/shahab33/GenFKD #Narval 
 
 
 module purge
@@ -24,11 +24,11 @@ module load cuda
 
 #source /home/shahab33/FeDK2P/bin/activate  	# Cedar
 
-source /home/shahab33/fed2p/bin/activate #Graham
+#source /home/shahab33/fed2p/bin/activate #Graham
 
-#source /home/shahab33/fed2p/bin/activate #Narval
+source /home/shahab33/fed2p/bin/activate #Narval
 
 #python main1.py --local_model_name "ResNet20" --num_train_samples 10000 --alpha_dirichlet 10 --output_name "ResNet20_10K_alpha10_"  	# this is the direction and the name of your code
-#python main.py --local_model_name "ResNet18" --num_train_samples 50000 --alpha_dirichlet 100 --output_name "ResNet18_50K_alpha100_"
+python main.py --local_model_name "ResNet18" --num_train_samples 10000 --alpha_dirichlet 100 --output_name "ResNet18_10K_alpha100_"
 
-python ImgGen.py
+#python ImgGen.py
