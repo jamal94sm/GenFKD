15cb3820eb5cbc8408651be13bba6a03072aa64d
Jamal Seyedmohammadi <44075279+jamal94sm@users.noreply.github.com>
2025-09-16 11:05:30 +0330

    Update MyUtils.py

--- MyUtils.py
+++ MyUtils.py
@@ -305,7 +305,7 @@ def adjust_temperature_orginal(inputs, iteration, optimal_temperature, is_softma
         probabilities = torch.nn.functional.softmax(inputs / optimal_temperature, dim=1)
     return probabilities, optimal_temperature
 ############################################################################################################## 
-def plot(arrays, names=[""], title=args.output_name, xlabel='rounds', ylabel='accuracy %', file_name="figure_"+args.output_name):
+def plot(arrays, names=[""], title='Comparison of Arrays', xlabel='rounds', ylabel='accuracy %', file_name="figure"):
     # Convert to numpy array with dtype=object to handle inhomogeneous sequences
     arrays = np.array(arrays, dtype=object)
 
