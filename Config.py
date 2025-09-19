755c55188f2015a4ab52ff56e3ef8ed44bdd319d
Jamal Seyedmohammadi <44075279+jamal94sm@users.noreply.github.com>
2025-09-20 02:03:36 +0330

    Update Config.py

--- Config.py
+++ Config.py
@@ -20,8 +20,8 @@ def get_args():
     parser.add_argument('--dataset', type=str, default="cifar10")
 
     parser.add_argument('--num_synth_img_per_class', type=int, default=100)
-    parser.add_argument('--num_prompts', type=int, default=4)
-    parser.add_argument('--global_epochs', type=int, default=5)
+    parser.add_argument('--num_prompts', type=int, default=10)
+    parser.add_argument('--global_epochs', type=int, default=1)
     
     parser.add_argument('--Foundation_model', type=str, default="openai/clip-vit-base-patch32")
     parser.add_argument('--rounds', type=int, default=30)
