bc9b66faa8019686aa3d8cf1f6657f5077ad4cca
Jamal Seyedmohammadi <44075279+jamal94sm@users.noreply.github.com>
2025-09-20 02:09:57 +0330

    Update GenFKD.sh

--- GenFKD.sh
+++ GenFKD.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4            # adjust (e.g., 8 or 16 if needed)
 #SBATCH --mem=40G                    # memory per node
-#SBATCH --time=10:00:00               # job time limit (HH:MM:SS)
+#SBATCH --time=2:00:00               # job time limit (HH:MM:SS)
 #SBATCH --mail-user=jamal73sm@gmail.com
 #SBATCH --mail-type=ALL
 
@@ -30,6 +30,6 @@ module load cuda
 source /home/shahab33/fed2p/bin/activate #Narval
 
 #python main1.py --local_model_name "ResNet20" --num_train_samples 10000 --alpha_dirichlet 10 --output_name "ResNet20_10K_alpha10_"  	# this is the direction and the name of your code
-python main.py --local_model_name "ResNet18" --num_train_samples 11000 --alpha_dirichlet 0.1 --rounds 40 --num_synth_img_per_class 100 --output_name "_ResNet18_10K_alpha0.1_synth100_T3_"
+python main.py --local_model_name "ResNet18" --num_train_samples 11000 --alpha_dirichlet 1 --rounds 30 --num_synth_img_per_class 100 --global_rounds 1 --num_prompts 4 --output_name "_ResNet18_10K_alpha1_synth100_np4_ge1_"
 
 #python ImgGen.py
