f0460ee2a1257a4290b2941d38a4f05969d4c0e6
Jamal Seyedmohammadi <44075279+jamal94sm@users.noreply.github.com>
2025-09-20 19:50:24 +0330

    Update GenFKD.sh

--- GenFKD.sh
+++ GenFKD.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4            # adjust (e.g., 8 or 16 if needed)
 #SBATCH --mem=40G                    # memory per node
-#SBATCH --time=10:00:00               # job time limit (HH:MM:SS)
+#SBATCH --time=2:00:00               # job time limit (HH:MM:SS)
 #SBATCH --mail-user=jamal73sm@gmail.com
 #SBATCH --mail-type=ALL
 
