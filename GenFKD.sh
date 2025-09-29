b64d01ae7120457f2332c3c539233dcddea91539
Jamal Seyedmohammadi <44075279+jamal94sm@users.noreply.github.com>
2025-09-29 13:33:04 +0330

    Update GenFKD.sh

--- GenFKD.sh
+++ GenFKD.sh
@@ -31,6 +31,6 @@ source /home/shahab33/fed2p/bin/activate #Narval
 
 #python main1.py --local_model_name "ResNet20" --num_train_samples 10000 --alpha_dirichlet 10 --output_name "ResNet20_10K_alpha10_"  	# this is the direction and the name of your code
 #python main.py --local_model_name "ResNet18" --dataset "SVHN" --num_train_samples 11000 --alpha_dirichlet 10 --rounds 30 --num_synth_img_per_class 100 --output_name "_ResNet18_10K_alpha10_synth100_T4"
-python main.py --local_model_name "EfficientNet" --dataset "cifar10" --num_train_samples 33000 --alpha_dirichlet 10 --rounds 50 --num_synth_img_per_class 300 --output_name "_EN_30K_alpha10_synth300_"
+python main.py --local_model_name "EfficientNet" --dataset "cifar10" --num_train_samples 33000 --alpha_dirichlet 0.1 --rounds 50 --num_synth_img_per_class 300 --output_name "_EN_30K_alpha0.1_synth300_"
 
 #python ImgGen.py
