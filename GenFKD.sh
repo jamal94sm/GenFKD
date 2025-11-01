0cf079c20acdcebc19e379fa7efbfed8a022cb30
Jamal Seyedmohammadi <44075279+jamal94sm@users.noreply.github.com>
2025-11-01 11:00:49 +0800

    Update GenFKD.sh

--- GenFKD.sh
+++ GenFKD.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1            # adjust (e.g., 8 or 16 if needed)
 #SBATCH --mem=40G                    # memory per node
-#SBATCH --time=00:50:00               # job time limit (HH:MM:SS)
+#SBATCH --time=06:00:00               # job time limit (HH:MM:SS)
 #SBATCH --mail-user=jamal73sm@gmail.com
 #SBATCH --mail-type=ALL
 
@@ -29,7 +29,7 @@ module load cuda
 
 source /home/shahab33/fed2p/bin/activate #Narval
 
-python main.py --local_model_name "ResNet18" --dataset "EuroSAT" --num_train_samples 11000 --alpha_dirichlet 10 --rounds 30 --num_synth_img_per_class 100 --output_name "_ResNet18_10K_alpha10_synth100"
+python main.py --local_model_name "ResNet18" --dataset "EuroSAT" --num_train_samples 11000 --alpha_dirichlet 10 --rounds 30 --num_synth_img_per_class 100 --output_name "_ResNet18_EuroSAT_20K_alpha10_synth100"
 #python main.py --local_model_name "ResNet18" --dataset "cifar10" --num_train_samples 33000 --alpha_dirichlet 10 --rounds 50 --num_synth_img_per_class 300 --output_name "_RN18_30K_alpha10_synth300_"
 
 
