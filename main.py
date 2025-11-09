5108ff002d0fdcf21937bc519b564289caeb01f2
Jamal Seyedmohammadi <44075279+jamal94sm@users.noreply.github.com>
2025-11-09 23:55:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ def main():
     
     # ===================== Build public dataset =====================
     
-    synth_img_dir = "/project/def-arashmoh/shahab33/FedPD/Synthetic_Image/Fashion"
+    synth_img_dir = args.synth_path
     #public_data = MyUtils.load_synthetic_images( name_classes, data_dir=synth_img_dir, max_per_class=args.num_synth_img_per_class)
     if "sidclip" in args.setup or "open_vocab" in args.setup: 
         public_data = MyUtils.load_synthetic_images(name_classes, 
@@ -352,7 +352,7 @@ if __name__ == "__main__":
     Dataset, num_classes, name_classes = MyDatasets.load_data_from_Huggingface()
     print("\n class names: \n", name_classes)
 
-
+'''
     name_classes = [
         "T-shirt",
         "Trouser",
@@ -365,7 +365,7 @@ if __name__ == "__main__":
         "Bag",
         "Ankle boot"
     ]
-
+'''
     # ===================== Data Distribution =====================
     distributed_dataset, num_samples = MyDatasets.data_distributing(Dataset, num_classes)
     print("\n ]data distribution of devices: \n", num_samples)
